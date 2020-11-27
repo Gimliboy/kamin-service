329f747effe7260c805c104bd3b4aedf5f0d642c
Gimliboy <kiesslingj.paul@gmail.com>
2020-11-27 18:51:58 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+const player = require('play-sound')(opts = {})
+
+// download mp3 part of youtube video 
+ytdl('https://www.youtube.com/watch?v=jrVXitpLGMk', {filter: 'audioonly'})
+  .pipe(fs.createWriteStream('./testVideo.mp3'));
+
+player.play('testVideo.mp3', (err) =>
+  {
+	  if(err)
+	  	{
+			  console.log("problem")
+		  }
+  })
