7e0478cea1de9b8a7cdbe1d7b04a5af212b84989
Gimliboy <kiesslingj.paul@gmail.com>
2020-12-18 13:10:20 +0100

    blabla

--- vue-interface/src/main.js
+++ vue-interface/src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
-
+//import speaker from '../node_modules/speaker';
+//import Decoder from '../node_modules/minimp3';
+import fs from 'fs';
+import ytdl from 'ytdl-core';
+console.log(fs)
 Vue.config.productionTip = false
+//Vue.prototype.$speaker = speaker;
+//Vue.prototype.$decoder = Decoder;
+Vue.prototype.$fs = fs;
+Vue.prototype.$ytdl = ytdl;
 
 new Vue({
   vuetify,
