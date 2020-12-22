5df493b3c6767a3208c59685411b548ec152281b
Gimliboy <kiesslingj.paul@gmail.com>
2020-12-22 01:31:53 +0100

    refactored

--- express-backend/index.js
+++ express-backend/index.js
@@ -1,3 +1,7 @@
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+const player = require('play-sound')(opts = {})
+
 import Express from 'express';
 
 const app = Express();
@@ -8,11 +12,19 @@ app.get("/", (req, res) => {
 })
 
 app.get("/downloadSong", (req, res) => {
-
+// download mp3 part of youtube video 
+ytdl('https://www.youtube.com/watch?v=AWKzr6n0ea0', {filter: 'audioonly'})
+  .pipe(fs.createWriteStream('/media/kaminSound2.mp3'));
 })
 
 app.get("/playSong", (req, res) => {
-
+player.play('testVideo.mp3', (err) =>
+  {
+	  if(err)
+	  	{
+			  console.log("problem")
+		  }
+  })
 })
 
 app.get("/songs", (req, res) => {
