772fef69929b741b1f3b9ee0c0a06854575a03ac
Gimliboy <kiesslingj.paul@gmail.com>
2020-12-22 11:19:27 +0100

    sth small

--- express-backend/index.js
+++ express-backend/index.js
@@ -1,34 +1,33 @@
-const fs = require('fs');
-const ytdl = require('ytdl-core');
-const player = require('play-sound')(opts = {})
+const fs = require("fs");
+const ytdl = require("ytdl-core");
+const player = require("play-sound")((opts = {}));
 
-import Express from 'express';
-
+import Express from "express";
+const youtubeUrl = "https://www.youtube.com/";
+const mediaUrl = "/media/";
 const app = Express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("hi")
-})
+  res.send("hi");
+});
 
 app.get("/downloadSong", (req, res) => {
-// download mp3 part of youtube video 
-ytdl('https://www.youtube.com/watch?v=AWKzr6n0ea0', {filter: 'audioonly'})
-  .pipe(fs.createWriteStream('/media/kaminSound2.mp3'));
-})
+  // download mp3 part of youtube video
+  ytdl(youtubeUrl + "watch?v=AWKzr6n0ea0", {
+    filter: "audioonly",
+  }).pipe(fs.createWriteStream(mediaUrl + "kaminSound2.mp3"));
+});
 
 app.get("/playSong", (req, res) => {
-player.play('testVideo.mp3', (err) =>
-  {
-	  if(err)
-	  	{
-			  console.log("problem")
-		  }
-  })
-})
+  // play the song
+  player.play(mediaUrl + "testVideo.mp3", (err) => {
+    if (err) {
+      console.log("problem");
+    }
+  });
+});
 
-app.get("/songs", (req, res) => {
+app.get("/songs", (req, res) => {});
 
-})
-
-app.listen(port, () => console.log("listening on http://localhost:" + port))
+app.listen(port, () => console.log("listening on http://localhost:" + port));
